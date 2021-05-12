9742cefccfe2ea4bc38d389805adeb8ae09bd6b6
pmaycomia <pmaycomia@gmail.com>
2021-05-12 17:53:24 +0200

    Close nav menu after clicking links

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const mainMenu = document.querySelector('.mainMenu')
 const openMenu = document.querySelector('.openMenu')
 const closeMenu = document.querySelector('.closeMenu')
-
+const link1 = document.querySelector('.link1')
+const link2 = document.querySelector('.link2')
+const link3 = document.querySelector('.link3')
 
 openMenu.addEventListener('click', show)
 closeMenu.addEventListener('click', close)
-
+link1.addEventListener('click', close)
+link2.addEventListener('click', close)
+link3.addEventListener('click', close)
 
 function show() {
     mainMenu.style.display = 'flex'
@@ -18,4 +22,4 @@ function close() {
     console.log("close");
 }
 
-//close menu as  soon as clicked
+//close menu as  soon as clicked 
